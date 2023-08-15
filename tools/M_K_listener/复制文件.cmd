ee4eabaf15c02593e5f1cee4fb2209eb8888a45c
张博超 <376307426@qq.com>
2023-08-15 16:42:36 +0800

    Update 复制文件.cmd

--- tools/M_K_listener/复制文件.cmd
+++ tools/M_K_listener/复制文件.cmd
@@ -6,16 +6,13 @@ set /p input=
 set OutDir=%input%Out
 
 :: /s 是代表删除所有子目录跟其中的档案 /q是不要它在删除档案或目录时，不再问我 Yes or No 的动作
-rmdir /s/q %OutDir%
+:: rmdir /s/q %OutDir%
+:: mkdir %OutDir%
 
-mkdir %OutDir%
-
-:: /e复制目录和子目录，包括空的 /y强制复制
-xcopy /e/y src\main\java %OutDir%\
+:: /e复制目录和子目录，包括空的 /y强制复制 /i表示如果目标目录不存在，将会被创建。
+xcopy /e/y/i src\main\java %OutDir%\
 
 xcopy /y Config.properties %OutDir%\
 xcopy /y 1.png %OutDir%\
 
-xcopy /y %input%\Functions.java %OutDir%\
-
-xcopy /y %input%\Config.properties %OutDir%\
+xcopy /y/i %input% %OutDir%
