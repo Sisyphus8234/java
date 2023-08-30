9795c796b49df15aac3fb8ea7793c49273776b27
张博超 <376307426@qq.com>
2023-08-30 18:04:28 +0800

    Update Functions游侠.java

--- 1b tools/M_K_listener/Diablo4/Functions游侠.java
+++ 1b tools/M_K_listener/Diablo4/Functions游侠.java
@@ -16,22 +16,20 @@ public class Functions游侠 extends Functions公共 {
 
 //    public static Long baseDelay = Long.parseLong(Config.read("BaseDelay"));
     public static Long baseDelay = 150L;
-    public static boolean t1Temp = false;
-    public static boolean t1Temp1 = false;
-    public static boolean t2Temp = false;
-    public static boolean t2Temp1 = false;
+
+
     public static boolean t3Temp = false;
 
     public static boolean 右键或者1234在t1运行时按下 = false;
     public static boolean 之前已经有右键或者1234在t1运行时按下 = false;
-    public static boolean 暂停t1时是否松开左键 = true;
+    public static boolean w或者左键 = false;
     public static boolean 是否基础技能 = true;
     public static boolean 是否核心技能 = true;
     public static 筛选装备_游侠 筛选装备_游侠 = new 筛选装备_游侠();
-    public static MyThread t1;
-    public static MyThread t2;
+
+
     public static MyThread t3;
-    public static MyThread t4;
+
 
     public static final int rgby1= 980;
     public static final int rgby2= 995;
@@ -45,89 +43,102 @@ public class Functions游侠 extends Functions公共 {
     public static float[] myHSB资源;
 
 
+    public static boolean t1Temp = false;
+    public static boolean t1Temp1 = false;
+    public static MyThread t1 = new MyThread(MyThread.State.on) {
+        @Override
+        public void run() {
+            while (true) {
+                if (t1Temp == true) {
+
+                    if(是否基础技能) {
+
+                        myHSB资源 = pixelColor.getPixelColor(rgbx,rgby);
+                        if(myHSB资源[0]<value)
+                        {
+                            robot.mouseRelease(BUTTON1_DOWN_MASK);
+                            robot.mousePress(BUTTON1_DOWN_MASK);
+                        }else {
+                        }
+
+
+                    }
+                    robot.keyRelease(VK_5);
+                    robot.keyPress(VK_5);
+
+                    t1Temp1=true;
+
+                } else {
+                    if(t1Temp1==true){
+                        robot.keyRelease(VK_5);
+                        if(w或者左键==false) {
+                            robot.mouseRelease(BUTTON1_DOWN_MASK);
+                        }
+                        t1Temp1=false;
+                    }
+                }
+                pause(baseDelay);
+            }
+        }
+    };
+    @ListenMouseKeyboard(value = 513, keyboardOrMouse = ListenMouseKeyboard.KeyboardOrMouse.Keyboard)
+    @ListenMouseKeyboard(value = 87, keyboardOrMouse = ListenMouseKeyboard.KeyboardOrMouse.Keyboard)
+    public static void w() {
+        w或者左键 = true;
+        t1Temp = false;
+        Functions公共.自动喝药=false;
+        t2Temp = false;
+    }
+    @ListenMouseKeyboard(value = 513, press = false,keyboardOrMouse = ListenMouseKeyboard.KeyboardOrMouse.Keyboard)
+    @ListenMouseKeyboard(value = 87, press = false,keyboardOrMouse = ListenMouseKeyboard.KeyboardOrMouse.Keyboard)
+    public static void w_1() {
+        w或者左键 = false;
+    }
+    @ListenMouseKeyboard(note = "e",value = 69, intercept = true, keyboardOrMouse = ListenMouseKeyboard.KeyboardOrMouse.Keyboard)
+    public static void e() {
+        是否基础技能=false;
+        t1Temp = true;
+        t2Temp = false;
+        Functions公共.自动喝药=true;
+        Functions公共.t1.myResume();
+    }
+    @ListenMouseKeyboard(note = "e",value = 69,press = false, intercept = true, keyboardOrMouse = ListenMouseKeyboard.KeyboardOrMouse.Keyboard)
+    public static void e1() {
+        是否基础技能=true;
+        t1Temp = true;
+        t2Temp = false;
+    }
+
+
+    public static boolean t2Temp = false;
+    public static boolean t2Temp1 = false;
+    public static MyThread t2 = new MyThread(MyThread.State.on) {
+        @Override
+        public void run() {
+            while (true) {
+//
+                if (t2Temp == true) {
+                    robot.keyPress(VK_G);
+                    robot.keyRelease(VK_G);
+                }
+                pause(baseDelay);
+
+            }
+        }
+    };
+
+    @ListenMouseKeyboard(value = 82, intercept = true, keyboardOrMouse = ListenMouseKeyboard.KeyboardOrMouse.Keyboard)
+    public static void R() {
+        t1Temp = false;
+//        Functions公共.自动喝药=false;
+        t2Temp = true;
+    }
+
 
     static {
-
 //        Controller.printKey=true;
 
-        t1 = new MyThread(MyThread.State.on) {
-            @Override
-            public void run() {
-                while (true) {
-                    if (t1Temp == true) {
-
-                        if(是否基础技能) {
-
-                            myHSB资源 = pixelColor.getPixelColor(rgbx,rgby);
-                            if(myHSB资源[0]<value)
-                            {
-                                robot.mouseRelease(BUTTON1_DOWN_MASK);
-                                robot.mousePress(BUTTON1_DOWN_MASK);
-                            }else {
-                            }
-
-
-
-
-//                            System.out.println(myHSB资源[0]);
-//                            System.out.println(myHSB资源[1]);
-//                            System.out.println(myHSB资源[2]);
-//                            if(myHSB资源[1]>test1){
-//                                test1 = myHSB资源[1];
-//                            }
-//                            System.out.println(test1);
-//                            System.out.println("-------------------------");
-
-
-
-
-//                            System.out.println(pixelColor);
-//                            if((pixelColor.getRed() +pixelColor.getGreen() +pixelColor.getBlue())<rgb){
-//                                rgb=pixelColor.getRed() +pixelColor.getGreen() +pixelColor.getBlue();
-//                            }
-//                            System.out.println(rgb);
-
-
-
-
-
-                        }
-                        robot.keyRelease(VK_5);
-                        robot.keyPress(VK_5);
-
-                        t1Temp1=true;
-
-                    } else {
-                        if(t1Temp1==true){
-                        robot.keyRelease(VK_5);
-                        robot.mouseRelease(BUTTON1_DOWN_MASK);
-                        t1Temp1=false;
-                        }
-
-
-                    }
-                    pause(baseDelay);
-                }
-            }
-        };
-
-        t2 = new MyThread(MyThread.State.on) {
-            @Override
-            public void run() {
-                while (true) {
-//
-                    if (t2Temp == true) {
-                        robot.keyPress(VK_G);
-                        robot.keyRelease(VK_G);
-                    }
-                    pause(baseDelay);
-
-                }
-            }
-        };
-
-
-        new MyThread(MyThread.State.on) {
+        t2=new MyThread(MyThread.State.on) {
             @Override
             public void run() {
                 while (true) {
@@ -148,10 +159,7 @@ public class Functions游侠 extends Functions公共 {
                 }
             }
         };
-
-
-
-        t3 = new MyThread(MyThread.State.on) {
+        t3=new MyThread(MyThread.State.on) {
             @Override
             public void run() {
                 while (true) {
@@ -171,64 +179,11 @@ public class Functions游侠 extends Functions公共 {
             }
         };
 
-        t4 = new MyThread(MyThread.State.off) {
-            @Override
-            public void run() {
-                while (true) {
-                    if(筛选装备.是否标记 ==true){
-                        筛选装备.run1();
-                    }else if(筛选装备.是否扫描和筛选 ==true){
-                        筛选装备.run(robot,筛选装备_游侠);
-                    }
 
-                    this.mySuspend();
-                }
-            }
-        };
 
     }
 
 
-
-    @ListenMouseKeyboard(value = 513, keyboardOrMouse = ListenMouseKeyboard.KeyboardOrMouse.Keyboard)
-    @ListenMouseKeyboard(value = 87, keyboardOrMouse = ListenMouseKeyboard.KeyboardOrMouse.Keyboard)
-    public static void w() {
-
-        暂停t1时是否松开左键 = false;
-//        t1Temp1=false;
-        t1Temp = false;
-        Functions公共.自动喝药=false;
-
-        t2Temp = false;
-    }
-
-
-
-    @ListenMouseKeyboard(value = 69, intercept = true, keyboardOrMouse = ListenMouseKeyboard.KeyboardOrMouse.Keyboard)
-    public static void e() {
-        是否基础技能=false;
-        t1Temp = true;
-        t2Temp = false;
-        Functions公共.自动喝药=true;
-        Functions公共.t1.myResume();
-    }
-
-    @ListenMouseKeyboard(value = 69,press = false, intercept = true, keyboardOrMouse = ListenMouseKeyboard.KeyboardOrMouse.Keyboard)
-    public static void e1() {
-        是否基础技能=true;
-        t1Temp = true;
-        t2Temp = false;
-    }
-
-    @ListenMouseKeyboard(value = 82, intercept = true, keyboardOrMouse = ListenMouseKeyboard.KeyboardOrMouse.Keyboard)
-    public static void R() {
-//        robot.keyRelease(VK_5);
-//        robot.keyRelease(BUTTON1_DOWN_MASK);
-
-        t1Temp = false;
-//        Functions公共.自动喝药=false;
-        t2Temp = true;
-    }
 
 
     @ListenMouseKeyboard(value = 523, keyboardOrMouse =     ListenMouseKeyboard.KeyboardOrMouse.Mouse)
@@ -323,25 +278,40 @@ public class Functions游侠 extends Functions公共 {
 
 
 
+
+
+
+
+    public static MyThread t4= new MyThread(MyThread.State.off) {
+        @Override
+        public void run() {
+            while (true) {
+                if(筛选装备.是否标记 ==true){
+                    筛选装备.run1();
+                }else if(筛选装备.是否扫描和筛选 ==true){
+                    筛选装备.run(robot,筛选装备_游侠);
+                }
+
+                this.mySuspend();
+            }
+        }
+    };
     @ListenMouseKeyboard(note = "f1",value = 112, keyboardOrMouse = ListenMouseKeyboard.KeyboardOrMouse.Keyboard, intercept = true)
     public static void 图像识别_装备() {
         筛选装备.标记终点();
     }
-
     @ListenMouseKeyboard(note = "f2",value = 113, keyboardOrMouse = ListenMouseKeyboard.KeyboardOrMouse.Keyboard)
     public static void 图像识别_装备1() {
         筛选装备.是否扫描和筛选 = true;
         筛选装备.是否标记 = false;
         t4.myResume();
     }
-
     @ListenMouseKeyboard(note = "f3",value = 114, keyboardOrMouse = ListenMouseKeyboard.KeyboardOrMouse.Keyboard, intercept = true)
     public static void 图像识别_装备2() {
         筛选装备.是否扫描和筛选 = false;
         筛选装备.是否标记 = true;
         t4.myResume();
     }
-
     @ListenMouseKeyboard(note = "f4",value = 115, keyboardOrMouse = ListenMouseKeyboard.KeyboardOrMouse.Keyboard, intercept = true)
     public static void 图像识别_装备_终止() {
         筛选装备.是否扫描和筛选 = false;
