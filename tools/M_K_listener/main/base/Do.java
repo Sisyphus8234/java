5d65cf01cf05656977c73cd35eb2862640177862
张博超 <376307426@qq.com>
2024-03-13 11:32:44 +0800

    Update Do.java

--- tools/M_K_listener/main/base/Do.java
+++ tools/M_K_listener/main/base/Do.java
@@ -1,81 +1,74 @@
 package base;
 
+import java.lang.reflect.InvocationTargetException;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Do
-{
+public class Do {
 
-	public long refreshtime;
-	public List<TaskInfo> taskInfoList =new ArrayList<>();
+    public long refreshtime;
+    public List<TaskInfo> taskInfoList = new ArrayList<>();
 
-	public static Object object;
-	
-		
-	
-	public Do(long refreshtime) {
-		this.refreshtime=refreshtime;
-		
-		new Thread() {
-		    @Override
-		    public void run() {
-		    	while(true) {try {Thread.sleep(refreshtime);} catch (InterruptedException e) {e.printStackTrace();}
-	            delay(taskInfoList);
-	    }}}.start();
+    public static Object object;
+
+
+    public Do(long refreshtime) {
+        this.refreshtime = refreshtime;
+
+        new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        Thread.sleep(refreshtime);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    delay(taskInfoList);
+                }
+            }
+        }.start();
     }
 
 
-	public void doTask(TaskInfo taskInfo) {
+    public void doTask(TaskInfo taskInfo) {
 
-		if(Duration.between(taskInfo.lastTime, LocalDateTime.now()).toMillis()>taskInfo.inputInfo.timeInterval) {
-			taskInfo.lastTime=LocalDateTime.now();
-			if (taskInfo.immediately == true) {
-				//立即执行功能
-				this.immediate(taskInfo);
-			} else if (taskInfo.immediately == false) {
-				//放入队列执行
-				this.taskInfoList.add(taskInfo);
-			}
-		}
+        if (Duration.between(taskInfo.lastTime, LocalDateTime.now()).toMillis() > taskInfo.inputInfo.timeInterval) {
+            taskInfo.lastTime = LocalDateTime.now();
+            if (taskInfo.immediately == true) {
+                //立即执行功能
+                this.invoke(taskInfo);
+            } else if (taskInfo.immediately == false) {
+                //放入队列执行
+                this.taskInfoList.add(taskInfo);
+            }
+        }
+    }
 
-	}
+    private void invoke(TaskInfo taskInfo) {
+        try {
+            int parameterCount = taskInfo.method.getParameterCount();
+            if (parameterCount > 0) {
+                taskInfo.taskResult = (TaskResult) taskInfo.method.invoke(object, taskInfo.inputInfoActualTemp);
+            } else {
+                taskInfo.taskResult = (TaskResult) taskInfo.method.invoke(object);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void delay(List<TaskInfo> taskInfoList) {
+        if (taskInfoList.size() > 0) {
+            TaskInfo taskInfo = taskInfoList.get(0);
+
+            invoke(taskInfo);
+
+            taskInfoList.remove(0);
+        }
+    }
 
 
-	public void immediate(TaskInfo taskInfo) {
-
-		try {
-			int parameterCount = taskInfo.method.getParameterCount();
-			if (parameterCount > 0) {
-				taskInfo.taskResult = (TaskResult) taskInfo.method.invoke(object, taskInfo.inputInfoActualTemp);
-			} else {
-				taskInfo.taskResult = (TaskResult) taskInfo.method.invoke(object);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-	
-	public void delay(List<TaskInfo> taskInfoList) {
-		if(taskInfoList.size()>0) {
-			try {
-				TaskInfo taskInfo = taskInfoList.get(0);
-				int parameterCount = taskInfo.method.getParameterCount();
-				if(parameterCount>0){
-					taskInfo.method.invoke(object, taskInfo.inputInfo);
-				}
-				taskInfo.method.invoke(object);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-
-			taskInfoList.remove(0);
-	    }
-	}
-
-	
-	
 }
