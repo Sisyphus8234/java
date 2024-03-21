c754f11a1b6b93637dcf43213ec163a7a2d49ac4
张博超 <376307426@qq.com>
2024-03-21 08:47:52 +0800

    Update MouseMoveFix.java

--- tools/M_K_listener/main/addition/MouseMoveFix.java
+++ tools/M_K_listener/main/addition/MouseMoveFix.java
@@ -3,8 +3,16 @@ package addition;
 import static base.IFunctions.robot;
 
 public class MouseMoveFix {
-    public static void run(int x,int y,double screenScale){
-        robot.mouseMove(-1,-1);
-        robot.mouseMove((int) (x/screenScale), (int) (y/screenScale));
+    public static void run(int x, int y, double screenScale) {
+//        //1.8fix
+//        robot.mouseMove(-1,-1);
+//        robot.mouseMove((int) (x/screenScale), (int) (y/screenScale));
+        //17
+        robot.mouseMove(x, y);
+    }
 
-    }}
+    public static void run(int x, int y) {
+        //17
+        robot.mouseMove(x, y);
+    }
+}
