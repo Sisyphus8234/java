a0b4fe08b5b2ceb52f6c2c88f870f88d7962c5ec
张博超 <376307426@qq.com>
2024-08-08 11:00:55 +0800

    Update IFunctions.java

--- tools/M_K_listener/main/base/IFunctions.java
+++ tools/M_K_listener/main/base/IFunctions.java
@@ -1,15 +1,15 @@
 package base;
 
-import java.awt.AWTException;
-import java.awt.Robot;
+import java.awt.*;
 import java.awt.datatransfer.*;
+import java.awt.geom.AffineTransform;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
 import base.CommonUtil.Active;
 
 
-import java.awt.Toolkit;
 import java.util.Map;
 
 
@@ -42,6 +42,34 @@ public class IFunctions {
 
     }
 
+//    public static double screen_scale = Double.parseDouble(Config.read("screen_scale"));
+
+    public static Toolkit toolkit = Toolkit.getDefaultToolkit();
+
+    //缩放比例
+    public static AffineTransform affineTransform = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice().getDefaultConfiguration().getDefaultTransform();
+    public static double scaleX = affineTransform.getScaleX();
+    public static double scaleY = affineTransform.getScaleY();
+    public static double screen_scale;
+    static {
+        if(scaleX==scaleY){
+            screen_scale=scaleX;
+        }else {
+            throw new RuntimeException("屏幕缩放吧比例x与y不相等");
+        }
+        System.out.println(screen_scale);
+    }
+
+    //屏幕尺寸
+    public static Dimension screenSize = toolkit.getScreenSize();
+    public static int screenWidth = (int) (screenSize.getWidth()*screen_scale);
+    public static int screenHeight = (int) (screenSize.getHeight()*screen_scale);
+    static{
+        System.out.println(screenWidth);
+        System.out.println(screenHeight);
+
+    }
+
     public static void pause(long millis) {
         try {
             Thread.sleep(millis);
@@ -51,7 +79,6 @@ public class IFunctions {
     }
 
 
-    public static Toolkit toolkit = Toolkit.getDefaultToolkit();
     public static Clipboard clipboard = toolkit.getSystemClipboard();
 
 
@@ -89,30 +116,33 @@ public class IFunctions {
         }
     }
 
-    private static Map<Integer,Boolean> keyStatusMap=new HashMap<>();
+    private static Map<Integer, Boolean> keyStatusMap = new HashMap<>();
 
-    public static void myKeyPress(int key){
+    public static void myKeyPress(int key) {
         robot.keyPress(key);
-        keyStatusMap.put(key,true);
+        keyStatusMap.put(key, true);
     }
-    public static void myKeyRelease(int key){
+
+    public static void myKeyRelease(int key) {
         robot.keyRelease(key);
-        keyStatusMap.put(key,false);
+        keyStatusMap.put(key, false);
     }
 
-    public static void myMousePress(int key){
+    public static void myMousePress(int key) {
         robot.mousePress(key);
-        keyStatusMap.put(key,true);
-    }
-    public static void myMouseRelease(int key){
-        robot.mouseRelease(key);
-        keyStatusMap.put(key,false);
+        keyStatusMap.put(key, true);
     }
 
-    public static boolean getKeyStatus(int key){
+    public static void myMouseRelease(int key) {
+        robot.mouseRelease(key);
+        keyStatusMap.put(key, false);
+    }
+
+    public static boolean getKeyStatus(int key) {
         return keyStatusMap.getOrDefault(key, false);
     }
-    public static void setKeyStatus(int key,boolean b){
+
+    public static void setKeyStatus(int key, boolean b) {
         keyStatusMap.put(key, b);
     }
 
