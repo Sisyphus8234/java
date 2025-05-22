5da613669c34b04b14e60570a5988ed77d47e8c6
张博超 <376307426@qq.com>
2025-05-22 10:35:40 +0800

    11

--- tools/ControlFrame/FakeMouse/FakeMouse.java
+++ tools/ControlFrame/FakeMouse/FakeMouse.java
@@ -211,43 +211,7 @@ public interface FakeMouse {
     public final static String alt右 = "altr";
     AtomicReference<LocalDateTime> time = new AtomicReference<>(LocalDateTime.now());
 
-    @ListenMouseKeyboard(extend = true, key = "alt左", keyboardOrMouse = CommonUtil.KeyboardOrMouse.Keyboard, customConditionReverse = alt左)
-    public static void alt左() {
-        customConditionSet.add(alt左);
 
-<<<<<<< HEAD
-=======
-        customConditionSet.remove(开关开);
->>>>>>> 9a7f284869f15e1b5ee242535c7885dc24cff7e1
-
-    }
-
-    @ListenMouseKeyboard(extend = true, key = "alt左", press = false, keyboardOrMouse = CommonUtil.KeyboardOrMouse.Keyboard)
-    public static void alt左1() {
-        customConditionSet.remove(alt左);
-<<<<<<< HEAD
-=======
-    }
-
-    @ListenMouseKeyboard(extend = true, key = "space", keyboardOrMouse = CommonUtil.KeyboardOrMouse.Keyboard, customCondition = alt左)
-    public static TaskResult space() {
-        customConditionSet.add(开关开);
-        return new TaskResult(true);
->>>>>>> 9a7f284869f15e1b5ee242535c7885dc24cff7e1
-    }
-
-
-    @ListenMouseKeyboard(extend = true, key = "alt右", keyboardOrMouse = CommonUtil.KeyboardOrMouse.Keyboard, customConditionReverse = alt右)
-    public static void alt右() {
-        customConditionSet.add(alt左);
-
-
-    }
-
-    @ListenMouseKeyboard(extend = true, key = "alt右", press = false, keyboardOrMouse = CommonUtil.KeyboardOrMouse.Keyboard)
-    public static void alt右1() {
-        customConditionSet.remove(alt左);
-    }
 
     //endregion
 }
